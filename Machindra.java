a2438600969b64f5ec03918a345857d80f0b1916
MachindraChitale <155707916+MachindraChitale@users.noreply.github.com>
2025-04-10 17:13:33 +0530

    EduMentor backend source code

--- Machindra.java
+++
@@ -1,5 +0,0 @@
-public class Machindra{
-    public static void main(String[] args) {
-        System.out.println("Machinra from this side");
-    }
-}
--- sachin.java
+++
@@ -1,9 +0,0 @@
-package EduMentor_dummy_project;
-class sachin{
-  public static void main (String[] args) {
-    System.out.println("Machindra from this side");
-    System.out.println("Vishwajeet from this side");
-    System.ou.ptinln("Sachin from this side");
-                       
-  }
-}
