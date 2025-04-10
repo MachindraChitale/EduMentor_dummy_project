b553a24a7def1c2f4ee2e218ae2bdfb9a84a71c8
VishwajeetDarekar <152512325+VishwajeetDarekar@users.noreply.github.com>
2025-04-10 12:44:37 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ package EduMentor_dummy_project;
 class Hello{
   public static void main (String[] args) {
     System.out.println("Machindra from this side");
+    System.out.println("Vishwajeet from this side");
                        
   }
 }
